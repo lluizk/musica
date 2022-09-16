d10a78bff610b58d714a37b3ce70c51e452a33aa
Sergio Luiz <sergioj995@hotmail.com>
2022-09-16 15:10:15 -0300

    musica finalizada

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿//perdão pelo atraso, fiz hoje pois estou de folga :(
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Red;
+
+System.Console.WriteLine("Musica: Hino do Flamengo");
+Thread.Sleep(2000); 
+System.Console.WriteLine();
+
+System.Console.WriteLine("Uma vez Flamengo, sempre Flamengo");
+System.Console.WriteLine("Flamengo sempre eu hei de ser");
+System.Console.WriteLine("É o meu maior prazer vê-lo brilhar");
+System.Console.WriteLine("Seja na terra, seja no mar");
+System.Console.WriteLine("Vencer, vencer, vencer");
+System.Console.WriteLine("Uma vez Flamengo, Flamengo até morrer");
+System.Console.WriteLine();
+Thread.Sleep(11000);
+
+Console.ForegroundColor = ConsoleColor.White;
+
+System.Console.WriteLine("Na regata ele me mata, me maltrata");
+System.Console.WriteLine("Me arrebata, que emoção no coração");
+System.Console.WriteLine("Consagrado no gramado, sempre amado");
+System.Console.WriteLine("Mais cotado no Fla-Flu, é o ai jesus");
+System.Console.WriteLine();
+Thread.Sleep(10000);
+
+System.Console.WriteLine("Eu teria um desgosto profundo");
+System.Console.WriteLine("Se faltasse o Flamengo no mundo");
+System.Console.WriteLine("Ele vibra, ele é fibra, muita libra, já pesou");
+System.Console.WriteLine("Flamengo até morrer, eu sou");
+System.Console.WriteLine();
+Thread.Sleep(10000);
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+System.Console.WriteLine("Uma vez Flamengo, sempre Flamengo");
+System.Console.WriteLine("Flamengo sempre eu hei de ser");
+System.Console.WriteLine("É o meu maior prazer vê-lo brilhar");
+System.Console.WriteLine("Seja na terra, seja no mar");
+System.Console.WriteLine("Vencer, vencer, vencer");
+System.Console.WriteLine("Uma vez Flamengo, Flamengo até morrer");
+System.Console.WriteLine();
+Thread.Sleep(10000);
+
+Console.ForegroundColor = ConsoleColor.White;
+
+System.Console.WriteLine("Na regata ele me mata, me maltrata");
+System.Console.WriteLine("Me arrebata, que emoção no coração");
+System.Console.WriteLine("Consagrado no gramado, sempre amado");
+System.Console.WriteLine("Mais cotado no Fla-Flu, é o ai jesus");
+System.Console.WriteLine();
+Thread.Sleep(10000);
+
+System.Console.WriteLine("Eu teria um desgosto profundo");
+System.Console.WriteLine("Se faltasse o Flamengo no mundo");
+System.Console.WriteLine("Ele vibra, ele é fibra, muita libra, já pesou");
+System.Console.WriteLine("Flamengo até morrer, eu sou");
+System.Console.WriteLine();
+
+Console.ResetColor();
+Console.ReadKey();
